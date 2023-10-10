a482b7c2d0937f1cedee2e0ca71f9debf6b13c4c
claudyadestine <144087128+claudyadestine@users.noreply.github.com>
2023-10-10 17:11:21 +0700

    Add files via upload

--- ForKelipatan08_1.java
+++ ForKelipatan08_1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ForKelipatan08_1 {
     public static void main(String[] args) {
-        Scanner input16 = new Scanner(System.in);
+        Scanner input08 = new Scanner(System.in);
 
         int kelipatan;
         int jumlah = 0;
@@ -10,16 +10,16 @@ public class ForKelipatan08_1 {
         double rataRata = 0.0;
 
         System.out.print("Masukkan bilanhgan kelipatan (1 - 9) : ");
-        kelipatan =  input16.nextInt();
+        kelipatan =  input08.nextInt();
 
         for (int i = 1; i <= 50; i ++) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter ++;
             } 
-            i ++;
-            rataRata = (double)
-            jumlah / counter;
+         }
+         if (counter > 0){
+            rataRata = (double) jumlah / counter;
          }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
